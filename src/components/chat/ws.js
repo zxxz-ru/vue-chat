494701971c6df074c863ad4b412b718144265613
zxxz <viacheslav.modestov@gmail.com>
2018-05-07 23:42:41 +0500

    socket discon in timeout and reconn on message

--- src/components/chat/ws.js
+++ src/components/chat/ws.js
@@ -4,7 +4,8 @@ install(Vue) {
         methods: {
       /* eslint-disable-next-line */
   init: function() {
-    // Vue.prototype.websocket = new WebSocket('ws://echo.websocket.org/');
+      /* eslint-disable-next-line */
+    Vue.prototype.websocket = new WebSocket('wss://echo.websocket.org/');
     this.websocket.onopen = (evt) => { this.onOpen(evt); };
     this.websocket.onclose = (evt) => { this.onClose(evt); };
     this.websocket.onmessage = (evt) => { this.onMessage(evt); };
@@ -14,6 +15,7 @@ install(Vue) {
       /* eslint-disable-next-line */
   onOpen: function (evt) {
     this.writeToScreen('Welcome to Echo Chat.', 'Server');
+    this.disconnectServer();
   },
 
       /* eslint-disable-next-line */
@@ -23,7 +25,15 @@ install(Vue) {
 
       /* eslint-disable-next-line */
   onMessage: function (evt) {
+      try {
+          if (evt.data === 'hh') {
+          throw new Error('bad word');
+          }
     this.writeToScreen(evt.data, 'Server');
+      } catch (e) {
+          // just to test try catch
+    this.writeToScreen(`O, this is a ${e.message}. Be polite, please.`, 'Server');
+      }
   },
 
       /* eslint-disable-next-line */
@@ -34,8 +44,19 @@ install(Vue) {
 
       /* eslint-disable-next-line */
   doSend: function (message) {
+      if (this.websocket.readyState !== 1) {
+      this.init();
+   this.websocket.onopen = () => {
+    this.cancel = false;
     this.writeToScreen(message, this.current_user);
     this.websocket.send(message);
+    this.disconnectServer();
+   };
+      } else {
+    this.cancel = false;
+    this.writeToScreen(message, this.current_user);
+    this.websocket.send(message);
+      }
   },
 
       /* eslint-disable-next-line */
@@ -51,10 +72,23 @@ install(Vue) {
               msg,
               });
   },
-            },
-        });
+      /* eslint-disable-next-line */
+    disconnectServer: async function() {
+    const p = new Promise(resolve =>
+        setTimeout(() => { resolve(true); }, 4000));
+        await p;
+        if (this.cancel) {
+            this.websocket.close();
+        } else {
+        this.cancel = !this.cancel;
+        this.disconnectServer();
+        }
+    },
+            }, // methods
+        }); // mixin
 
       /* eslint-disable-next-line */
-  Vue.prototype.websocket = new WebSocket('ws://echo.websocket.org/');
+//  Vue.prototype.websocket = new WebSocket('wss://echo.websocket.org/');
     },
+  cancel: true,
 };
